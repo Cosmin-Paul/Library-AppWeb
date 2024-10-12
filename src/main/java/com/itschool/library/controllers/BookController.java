cb8c6e44c30e3617cbb6f5b89e865dac279c9e92
Cosmin <pcosmin_17@yahoo.com>
2024-10-12 18:14:11 +0300

    Library updates

---
+++ src/main/java/com/itschool/library/controllers/BookController.java
@@ -0,0 +1,39 @@
+package com.itschool.library.controllers;
+
+import com.itschool.library.models.dtos.CopiesAvailableDTO;
+import com.itschool.library.models.dtos.RequestBookDTO;
+import com.itschool.library.models.dtos.ResponseBookDTO;
+import com.itschool.library.services.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/books")
+@RestController
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseBookDTO> createBook(@RequestBody RequestBookDTO requestBookDTO) {
+        return ResponseEntity.ok(bookService.createBook(requestBookDTO));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ResponseBookDTO> updateBookCopies(@PathVariable Long id, @RequestBody CopiesAvailableDTO copiesAvailable) {
+        return ResponseEntity.ok(bookService.updateBookCopies(id, copiesAvailable.getCopiesAvailable()));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseBookDTO>> getBooks(
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "author", required = false) String author,
+            @RequestParam(value = "genre", required = false) String genre) {
+        return ResponseEntity.ok(bookService.getBooks(title, author, genre));
+    }
+}
--- src/main/java/com/itschool/library/models/entities/Book.java
+++ src/main/java/com/itschool/library/models/entities/Book.java
@@ -11,22 +11,14 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(name = "title")
     private String title;
-
     @Column(name = "author")
     private String author;
-
     @Column(name = "isbn")
     private String isbn;
-
     @Column(name = "genre")
     private String genre;
-
     @Column(name = "copies_available")
-    private int copiesAvailable;
-
-    //    @Column(name = "copies_available")
-//    private int copiesAvailable;
+    private Integer copiesAvailable;
 }
