0d960e58b030b8618d1f3973e51ed50701f75cb0
Cosmin <pcosmin_17@yahoo.com>
2024-10-14 18:42:25 +0300

    Library updates

--- src/test/java/com/itschool/library/services/BookService.java
+++
@@ -1,8 +0,0 @@
-package com.itschool.library.services;
-
-import com.itschool.library.models.dtos.RequestBookDTO;
-import com.itschool.library.models.dtos.ResponseBookDTO;
-
-public interface BookService {
-    ResponseBookDTO createBook(RequestBookDTO requestBookDTO);
-}
